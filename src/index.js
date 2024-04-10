695593b22b58967f02b39474b3ad3a8bb29e4f5b
Jannes <jannesaussems@gmail.com>
2024-04-10 22:49:51 +0200

    adding webpack and assest management, installed css and scss loader

--- src/index.js
+++ src/index.js
@@ -1,26 +1,87 @@
-import _ from "lodash";
-import "style.scss";
-
-const component = () => {
-  const element = document.createElement("div");
-
-  element.innerHTML = _.join([`Hello`, `Jannes`], ` `);
-
-  return element;
-};
-console.log(`Loadded`)
-const dialog = document.querySelectorAll("dialog");
-const showButton = document.getElementById("dialog-show-btn");
+const dialog = document.querySelector("dialog");
+const contact = document.getElementById("contact");
 const closeButton = document.getElementById("dialog-close-btn");
+const navLinks = document.querySelectorAll(".page__header__list__item");
+const navLinksMobile = document.querySelectorAll(".page__mobile-menu__list__item");
+const nameInput = document.getElementsByClassName("name-input");
+const mobileIcon = document.querySelector("div.page__mobile-menu__icon");
+const mobileMenuList = document.querySelector("ul.page__mobile-menu__list");
+let isShownMenu = false;
+const storySection = document.querySelector("div.page__story");
+const skillsSection = document.querySelector("div.page__skills");
+const portfolioSection = document.querySelector("div.page__portfolio");
 
-showButton.addEventListener("click", () => {
-  console.log(`CLICKED on show button`)
+contact.addEventListener("click", () => {
   dialog.showModal();
+  document.getElementById("dialog").scrollIntoView();
 });
 
-// "Close" button closes the dialog
 closeButton.addEventListener("click", () => {
   dialog.close();
 });
 
-document.body.appendChild(component());
+const handleNavLinkClick = (navlink) => {
+  const navlinkName = navlink.attributes.getNamedItem("name").value;
+  
+  switch (navlinkName) {
+    case "story":
+      storySection.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" });
+      break;
+    case "skills":
+      skillsSection.scrollIntoView({ behavior: "smooth", block: "start" });
+      break;
+    case "portfolio":
+      portfolioSection.scrollIntoView({ behavior: "smooth", block: "start" });
+      break;
+  }
+};
+
+navLinks.forEach((navlink) => {
+  navlink.addEventListener("click", () => {
+    handleNavLinkClick(navlink);
+  });
+});
+
+navLinksMobile.forEach((navlink) => {
+  navlink.addEventListener("click", () => {
+    handleNavLinkClick(navlink);
+  });
+});
+
+var isInViewport = function (elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const skillsElement = document.querySelector(".page__skills__list");
+function handleScrollDebounce() {
+  if(isInViewport(skillsElement)) {
+    skillsElement.classList.add('animation--skills-list');
+  } else {
+    skillsElement.classList.remove('animation--skills-list');
+  }
+}
+
+let debounceTimer;
+window.addEventListener('scroll', () => {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(handleScrollDebounce, 400);
+}, false);
+
+mobileIcon.addEventListener("click", () => {
+  isShownMenu = !isShownMenu;
+
+  console.log(`Chjecking ${navLinksMobile.length}`)
+  if(isShownMenu) 
+  {
+    mobileMenuList.classList.add("page__mobile-menu__list--show");
+  }
+  else {
+    mobileMenuList.classList.remove("page__mobile-menu__list--show");
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,26 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    entry: './src/index',
     mode: 'development',
-  entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Output Management',
-      }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-    ],
-  },
- 
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            }
+        ]
+    }
+    
+}
